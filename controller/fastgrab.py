9d2598e74497d09dd88844be34d5ff9202bc5fa4
Don Blair <donblair@gmail.com>
2020-01-19 11:16:56 -0500

    update

--- controller/fastgrab.py
+++ controller/fastgrab.py
@@ -1,32 +1,35 @@
 import serial
 
 PORT='/dev/ttyACM0'
-OUTFILE='out.csv'
-
-#f=open(OUTFILE,"w+")
-#f=open(OUTFILE,"a+")
 
 s=serial.Serial(PORT,115200,timeout=1)
 
 SIZE = 10
-NUM = 10
+NUM = 1
 RES = 12
-RATE = 5
+RATE = 10
+SAMP_SPEED = 4
+CONV_SPEED = 4
 
 # SET UP THE SAMPLING
 
 s.write('ADC.SET_SIZE '+str(SIZE)+'\n\r') # number of samples to grab in one go
 s.write('ADC.SET_NUM '+str(NUM)+'\n\r') # number of times to repeat the sample grab
 s.write('ADC.SET_RES '+str(RES)+'\n\r') # resolution
-s.write('ADC.SET_RATE '+str(RATE)+'\n\r') # resolution
+s.write('ADC.SET_CONV_SPEED '+str(CONV_SPEED)+'\n\r')
+s.write('ADC.SET_SAMP_SPEED '+str(SAMP_SPEED)+'\n\r')
+
+#s.write('ADC.SET_RATE '+str(RATE)+'\n\r') # resolution
 
 print("SIZE=",SIZE)
 print("NUM=",NUM)
 print("RES=",RES)
 print("RATE=",RATE)
+print("SAMP_SPEED=",SAMP_SPEED)
+print("CONV_SPEED=",CONV_SPEED)
 
-for i in range(0,3):
+for i in range(0,10):
     s.write('ADC.START\n\r')
     print(s.readline())
-    s.write('ADC.STOP\n\r')
+    #s.write('ADC.STOP\n\r')
 
